17b2eb5507f7875de0dbfd063145f31574d36302
march-dave <ysangy@gmail.com>
2018-02-12 20:56:06 -0500

    up to date

--- models/fishbase.js
+++ models/fishbase.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 
 var FishBase = mongoose.model('fisHackathonDB', {
-    // imageurl: String,
+    imageurl: String,
     like: String,
     dislike: String
 });
